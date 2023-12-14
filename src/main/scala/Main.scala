b40d22b825544289223a9fb5648bff4a64ded6a2
Windymelt <windymelt@3qe.us>
2023-12-14 23:51:41 +0900

    refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,12 +5,12 @@ val elbat = table.map(_.swap)
 
 def encrypt(key: String, plain: String): String =
   // 大文字に揃える
-  val p = plain.toUpperCase()
+  val plainUpper = plain.toUpperCase()
   val k0 = LazyList.from(key.toUpperCase())
-  def k: LazyList[Char] = k0 #::: k // infinitely repeat `k0`
+  def keyUpper: LazyList[Char] = k0 #::: keyUpper // infinitely repeat `k0`
 
   // kとpをzipし、それぞれの文字を数値に変換して足し合わせてmod 26を取る
-  val cipher = (k zip p).map: (k, p) =>
+  val cipher = (keyUpper zip plainUpper).map: (k, p) =>
     (table(k) + table(p)) % 26
   cipher.map(elbat(_)).mkString
 
